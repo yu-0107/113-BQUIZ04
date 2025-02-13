4aad35cc9fa29c2bd79b959be54fb5b6c06eb367
onion <a0966550619@gmail.com>
2025-02-13 10:44:37 +0800

    製作後台管理者登入後的權限功能顯示

--- back.php
+++ back.php
@@ -1,4 +1,8 @@
-<?php include_once "api/db.php" ?>
+<?php include_once "./api/db.php";
+$user=$Admin->find(['acc'=>$_SESSION['Admin']]);
+$pr=unserialize($user['pr']);
+
+?>
 <!DOCTYPE html
     PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <!-- saved from url=(0057)?do=admin -->
@@ -24,24 +28,28 @@
         <div id="left" class="ct">
             <div style="min-height:400px;">
                 <a href="?do=admin">管理權限設置</a>
-                <a href="?do=th">商品分類與管理</a>
-                <a href="?do=order">訂單管理</a>
-                <a href="?do=mem">會員管理</a>
-                <a href="?do=bot">頁尾版權管理</a>
-                <a href="?do=news">最新消息管理</a>
+                <?php
+              echo (in_array(1,$pr))?"<a href='?do=th'>商品分類與管理</a>":'';
+              echo (in_array(2,$pr))?"<a href='?do=order'>訂單管理</a>":'';
+              echo (in_array(3,$pr))?"<a href='?do=mem'>會員管理</a>":'';
+              echo (in_array(4,$pr))?"<a href='?do=bot'>頁尾版權管理</a>":'';
+              echo (in_array(5,$pr))?"<a href='?do=news'>最新消息管理</a>":'';
+            ?>
                 <a href="./api/logout.php?table=Admin" style="color:#f00;">登出</a>
             </div>
         </div>
         <div id="right">
-            <?php
-			$do=$_GET['do']??'admin';
-			$file="back/".$do.".php";
-			if(file_exists($file)){
-					include $file;
-			}else{
-					include "back/admin.php";
-			}
-			?>
+            <?php 
+        $do=$_GET['do']??'admin';
+        $file="back/".$do.".php";
+        if(file_exists($file)){
+            include $file;
+        }else{
+            include "back/admin.php";
+        }
+
+
+        ?>
         </div>
         <div id="bottom" style="line-height:70px; color:#FFF; background:url(icon/bot.png);" class="ct">
             頁尾版權 : </div>
