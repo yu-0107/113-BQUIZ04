20dbb319599eb3392896b6005a48faad68ee287a
onion <a0966550619@gmail.com>
2025-02-17 11:02:30 +0800

    根據分類顯示商品

--- api/save_item.php
+++ api/save_item.php
@@ -1,6 +1,6 @@
 <?php include_once "db.php";
 
-if(isset($_FILES['img']['tmp_name'])){
+if(!empty($_FILES['img']['tmp_name'])){
     move_uploaded_file($_FILES['img']['tmp_name'],"../img/".$_FILES['img']['name']);
     $_POST['img']=$_FILES['img']['name'];
 }
--- front/main.php
+++ front/main.php
@@ -11,7 +11,55 @@ if($typeId==0){
         $big=$Type->find($type['big_id']);
         $nav=$big['name'] ." > ". $type['name'];
     }
+    
 }
-
 ?>
 <h2><?=$nav;?></h2>
+<style>
+.item {
+    display: flex;
+    margin: 3px auto;
+    width: 85%;
+}
+
+.item div {
+    padding: 5px;
+    border: 1px solid white;
+}
+
+.item>div:nth-child(1) {
+    width: 40%;
+}
+
+.item>div:nth-child(2) {
+    width: 60%;
+}
+</style>
+<?php 
+if($typeId==0){
+    $rows=$Item->all();
+}else if($type['big_id']==0){
+    $rows=$Item->all(['big'=>$typeId]);
+}else{
+    $rows=$Item->all(['mid'=>$typeId]);
+}
+?>
+<?php
+foreach($rows as $row):
+?>
+<div class='item'>
+    <div class='pp'>
+        <a href="?do=detail&id=<?=$row['id'];?>">
+            <img src="./img/<?=$row['img'];?>" style="width:200px;height:160px">
+        </a>
+    </div>
+    <div>
+        <div class='tt ct'><?=$row['name'];?></div>
+        <div class='pp'>價錢:<?=$row['price'];?></div>
+        <div class='pp'>規格:<?=$row['spec'];?></div>
+        <div class='pp'>簡介:<?=mb_substr($row['intro'],0,20);?>...</div>
+    </div>
+</div>
+<?php
+endforeach;
+?>
